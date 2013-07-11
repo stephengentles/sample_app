d133372f9546f1c1185c9ca0c3bdd3af4f06e8e9
stephengentles <starryb7@gmail.com>
2013-07-11 04:52:43 -0500

    Finish Layout

--- Gemfile
+++ Gemfile
@@ -33,8 +33,8 @@ gem 'sass-rails', '4.0.0'
 gem 'uglifier', '2.1.1'
 gem 'coffee-rails', '4.0.0'
 gem 'jquery-rails', '2.2.1'
-gem 'turbolinks', '1.1.1'
-gem 'jbuilder', '1.0.2'
+gem 'turbolinks', '1.2.0'
+gem 'jbuilder', '1.4.2'
 
 group :doc do
   gem 'sdoc', '0.3.20', require: false
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,3 +33,16 @@ describe "Static pages" do
     it { should have_title(full_title('Contact')) }
   end
 end
+
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+end
+
